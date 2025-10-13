7d1d827d2881eba5cb6bcde343a224251ebe1dd5
Mhiw <103310488+Mhiw@users.noreply.github.com>
2025-10-13 21:53:46 +0200

    Test commit

--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,19 @@
-let entities = [
-	new Player(200, 200),
-];
+let entities = [];
+let player;
 
 let walls = [
-	new Collider(0, 0, 1, 400, "Static", null),
-	new Collider(0, 0, 400, 1, "Static", null),
+	new Collider(-16, 0, 16, 400, "Static", null),
+	new Collider(400, 0, 16, 400, "Static", null),
 ]
 
 function setup() {
-	//spawnEnemies(10);
-	createCanvas(400, 400);
+	createCanvas(640*SCALE, 360*SCALE);
+	noSmooth();
+	preloadAssets();
+
+	player = new Player(50, 50, playerImg);
+	gun = new Gun(player.x+10, player.y+10, gunImg);
+	entities.push(player);
 }
 
 function spawnEnemies(count) {
@@ -19,20 +23,41 @@ function spawnEnemies(count) {
 }
 
 function draw() {
-	background(20);
+	background(39, 24, 84);
+	scale(SCALE)
 
 	for(let i = 0; i < entities.length; i++) {
 		entities[i].update();
 	}
+
+	push();
+	applyCameraShake();
+
+	// Temporärt, ville mest bara kolla om camera shaken faktiskt fungerade, vi kanske inte äns ska ha kvar den lol. 
+	stroke(80);
+	for (let x = 0; x < width / SCALE; x += 16) {
+		line(x, 0, x, height / SCALE);
+	}
+	for (let y = 0; y < height / SCALE; y += 16) {
+		line(0, y, width / SCALE, y);
+	}
+	
+	player.update();
+	gun.followPlayer(player.x + 10, player.y + 10);
+	gun.update();
+
+	// Test
+
+	pop();
 }
 
 function mouseClicked() {
+	startCameraShake(200, 3);
 	let player = entities[0];
+	scale(SCALE);
+	
 	let dx = mouseX - player.x;
 	let dy = mouseY - player.x;
 	let angle = Math.atan2(dy, dx);
-	dx = cos(angle);
-	dy = sin(angle);
-	
-	entities.push(new Bullet(player.x + dx * 50, player.y + dy * 50, dx, dy));
+	entities.push(new Bullet(player.x, player.y, cos(angle), sin(angle), bulletImg));
 }
